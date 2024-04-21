fa4cfd6755640783325b8f8bd7cfef74892d68b1
sijune <sijune0525@gmail.com>
2024-04-21 13:26:07 +0900

    http -> https

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -10,6 +10,6 @@ async function getVideos(id:string) {
 export default async function MovieVideos({id}:{id:string}) {
   const videos = await getVideos(id);
   return <div className={styles.container}>
-    {videos.map(video => <iframe key={video.id} src={`http://youtube.com/embed/${video.key}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={video.name} />)}
+    {videos.map(video => <iframe key={video.id} src={`https://youtube.com/embed/${video.key}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={video.name} />)}
   </div>
 }
