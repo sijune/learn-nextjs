b6dfd2ebe47c49727b4e0d9fe90fe6a4470f8e82
sijune <sijune0525@gmail.com>
2024-04-21 13:21:54 +0900

    remove prefetch

--- components/movie.tsx
+++ components/movie.tsx
@@ -18,6 +18,6 @@ export default function Movie({title, id, poster_path}: IMovieProps) {
   }
   return  <div className={styles.movie}>
   <img src={poster_path} alt={title} onClick={onClick}/>
-  <Link prefetch href={`/movies/${id}`}>{title}</Link>
+  <Link href={`/movies/${id}`}>{title}</Link>
 </div>
 }
